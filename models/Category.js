6d6951a28d78efab2588e61db4f5a94a01fc0e5f
zachary chapman <zach.chapman24@gmail.com>
2024-02-17 00:23:58 -0500

    worked on product.js

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,22 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { isFullWidth } = require('validator');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    Category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
